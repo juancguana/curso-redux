7d6fcb2b4f8c2452c98b1d4e8845beb1c98003d5
Juan Guana <juanc.guana@gmail.com>
2021-09-11 18:41:29 -0500

    Updates

---
+++ precache-manifest.223eb432643aec19531e2e41225efd01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5961500cc3e931cdfeaf4fb7cb58af67",
+    "url": "/curso-redux/index.html"
+  },
+  {
+    "revision": "ed1c85d4c57c213926a1",
+    "url": "/curso-redux/static/css/main.7a6641ea.chunk.css"
+  },
+  {
+    "revision": "fbfa959af5f81328728b",
+    "url": "/curso-redux/static/js/2.79b270f2.chunk.js"
+  },
+  {
+    "revision": "ed1c85d4c57c213926a1",
+    "url": "/curso-redux/static/js/main.8056bae8.chunk.js"
+  },
+  {
+    "revision": "d6de940dfeff026f2f06",
+    "url": "/curso-redux/static/js/runtime~main.fee84827.js"
+  }
+]);
